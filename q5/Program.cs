345c6ec4ac95752ffc4805643ae3bbcdf909c5ba
sravanigudisa <gudisajayasravani@gmail.com>
2022-10-26 19:48:16 +0530

    5

---
+++ q5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r;
+            double c, area, pi = 3.14;
+            Console.WriteLine("enter radius");
+            r = Convert.ToInt32(Console.ReadLine());
+            area = pi * r * r;
+            c = 2 * pi * r;
+            Console.WriteLine("area of circle {0}",area);
+            Console.WriteLine("circumference of circle {0}",c);
+            Console.ReadLine();
+        }
+    }
+}
