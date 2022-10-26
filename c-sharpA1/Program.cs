f6450ec486d2e6e7a11c5dc55e163e776062acd1
sravanigudisa <gudisajayasravani@gmail.com>
2022-10-26 19:44:53 +0530

    -1

---
+++ c-sharpA1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+
+namespace c_sharpA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            int add, sub, mul, div;
+            Console.WriteLine("enter n1");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter n2");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+
+            add = n1 + n2;
+            sub = n1 - n2;
+            mul = n1 * n2;
+            div = n1 / n2;
+            Console.WriteLine("results:");
+            Console.WriteLine("Addition-{0}", add);
+            Console.WriteLine("Subtraction-{0}", sub);
+            Console.WriteLine("Multiplication-{0}", mul);
+            Console.WriteLine("division-{0}", div);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
