ca310542d2a6daf3a8e7d3d9510acbb5320718c6
sravanigudisa <gudisajayasravani@gmail.com>
2022-10-26 19:48:46 +0530

    6

---
+++ q6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q6
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book();
+            b.book_id = 100;
+            b.title = "Magazine";
+            b.price = 50;
+
+            Console.WriteLine("Details of the book");
+            Console.WriteLine("Id of the book:" + b.book_id);
+            Console.WriteLine("title of the book:" + b.title);
+            Console.WriteLine("price of the book:" + b.price);
+            //Console.WriteLine("type of the book");
+            Console.ReadKey();
+
+        }
+        enum bookType
+        {
+            Comic,
+            Novels,
+            Referenebook,
+            Misellonories
+
+        }
+
+        public struct Book
+        {
+            public int book_id;
+            public string title;
+            public int price;
+
+        }
+    }
+}
