fbbcb922692da0fdc1e09b844f4d93a0c4ee6a0f
sravanigudisa <gudisajayasravani@gmail.com>
2022-10-26 19:47:53 +0530

    3

---
+++ q3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace q3
+{ 
+
+        internal class Q3
+        {
+            static void Main(String[] args)
+            {
+                int[] number = { 10, 20, 30, 40 };
+                int sum = 0;
+                for (int i = 0; i < number.Length; i++) ;
+                {
+                    sum = sum + number[0];
+                }
+                Console.WriteLine("the sum is {0}", sum);
+            }
+
+
+        }
+
+
+    
+}
