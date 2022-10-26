2022432f09730922de5ca270c7949df29b5bad3d
sravanigudisa <gudisajayasravani@gmail.com>
2022-10-26 19:48:02 +0530

    4

---
+++ q4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, s;
+
+            Console.WriteLine("enter the number for a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the number for b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("before swapping a={0} and b={1}", a, b);
+
+            s = a;
+            a = b;
+            b = s;
+
+            Console.WriteLine("after swapping a={0} and b={1}", a, b);
+            Console.ReadLine();
+
+
+        }
+    }
+}
